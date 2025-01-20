1a4f9d313d8319782eecd1a4fbb96997a34557a4
Harsh Kumar <139524462+harshrajjj@users.noreply.github.com>
2025-01-21 00:03:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 3000, // Optional: Change the port if needed
+    strictPort: true, // Optional: Fail if the port is already in use
+  },
 })
